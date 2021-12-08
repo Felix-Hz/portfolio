11b247bdee77cf92ff58238ed715f4451ee7a6a8
Felix-Hz <feluhv@outlook.es>
2021-12-08 14:32:30 -0300

    topbar mobile first -- start

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -18,7 +18,13 @@ export default function Topbar() {
             <span> feluhv@gmail.com </span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"> </span>
+            <span className="line2"> </span>
+            <span className="line3"> </span>
+          </div>
+        </div>
       </div>
     </div>
   );
